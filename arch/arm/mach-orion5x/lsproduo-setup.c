cf6301dfa6e8a11e1e9f320394e7a2665173370e
Philippe Coval <rzr@gna.org>
2013-04-20 22:36:51 +0200

    arm: upgrade: api: -.boot_params +.atag_offset

--- arch/arm/mach-orion5x/lsproduo-setup.c
+++ arch/arm/mach-orion5x/lsproduo-setup.c
@@ -378,7 +378,7 @@ MACHINE_END
 #ifdef CONFIG_MACH_LINKSTATION_PRODUO_REV2
 MACHINE_START(LINKSTATION_PRODUO, "Buffalo Linkstation Pro Duo - Revision 2")
        /* Maintainer: Manuel Bernhardt <prodigy7@gmail.com> */
-       .boot_params    = 0x00000100,
+       .atag_offset    = 0x00000100,
        .init_machine   = lsproduo_init,
        .map_io         = orion5x_map_io,
         .init_early     = orion5x_init_early,
